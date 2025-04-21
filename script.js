313808257808e3bdf53f6772833211b7078e14bb
GabrielPTuga <ribeiroportugalgabriel@gmail.com>
2025-04-21 12:26:11 -0100

    Adicionando os elementos na tela

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const todoList = []; // Empty array
+const todoList = ["Gabriel","Batata"]; // Empty array
 
+renderTodoList();
+
+function renderTodoList() {
+    let todoListHTML = "";
+
+    for (let i = 0;i<todoList.length;i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo} <button onclick="">Remove</button></p> `;
+        todoListHTML += html;
+    }
+
+    console.log(todoListHTML)
+
+    document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+
+}
 
 function addTodo() {
     const inputElement = document.querySelector(".js-todo-input");
@@ -9,4 +25,5 @@ function addTodo() {
     console.log(todoList);
 
     inputElement.value = ""; // Resetar texto do text box
+    renderTodoList();
 }
