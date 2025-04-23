c0c2159e39930404d54555f64bd0943b2b7a3bd4
GabrielPTuga <ribeiroportugalgabriel@gmail.com>
2025-04-23 16:23:23 -0100

    Adicionando botão de remove e CSS

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const todoList = ["Gabriel","Batata"]; // Empty array
+const todoList = [{
+    name: "Faze almoço",
+    doDate: "2022-06-03"
+},{
+    name: "Estudar MD",
+    doDate: "2025-06-11"
+}]; // Empty array
 
 renderTodoList();
 
@@ -6,8 +12,14 @@ function renderTodoList() {
     let todoListHTML = "";
 
     for (let i = 0;i<todoList.length;i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo} <button onclick="">Remove</button></p> `;
+        const todoObj = todoList[i];
+        // const name = todoObj.name;
+        //const doDate = todoObj.doDate;
+        const {name, doDate} = todoObj
+        const html = `<div>${name}</div> <div>${doDate}</div> <button onclick="
+            todoList.splice(${i},1);
+            renderTodoList();
+        " class="remove-button js-remove">Remove</button> `;
         todoListHTML += html;
     }
 
@@ -21,7 +33,15 @@ function addTodo() {
     const inputElement = document.querySelector(".js-todo-input");
     const name = inputElement.value;
 
-    todoList.push(name);
+    const dateInput = document.querySelector(".js-box")
+    const doDate = dateInput.value
+
+    todoList.push({
+        //name: name,
+        //doDate: doDate,
+        name,
+        doDate
+    });
     console.log(todoList);
 
     inputElement.value = ""; // Resetar texto do text box
